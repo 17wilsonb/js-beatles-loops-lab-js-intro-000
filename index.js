57184f419ecf3db5d5cadcdba017dd1500417d99
Brett Wilson <wilson.3458@osu.edu>
2020-05-13 23:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = new array();
+  for(let i = 0; i < musicians.length; i++){
+    for(let j = 0; j < instruments.length; j++){
+      array.push(`${musicians[i]} plays ${instruments[j]}`);
+    }
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var array = new array();
+  let count = 0;
+  while (count < facts.length){
+    array.push(facts[count] + '!!!');
+    count++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(num){
+  var array = new array();
+  do {
+    array.push(`I love the Beatles!`);
+  } while ();
+}
