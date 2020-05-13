4cbdee9502183cf8496933a677366c2cb0d798ef
Brett Wilson <wilson.3458@osu.edu>
2020-05-13 23:06:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(let i = 0; i < musicians.length; i++){
-    for(let j = 0; j < instruments.length; j++){
-      array.push(`${musicians[i]} plays ${instruments[j]}`);
-    }
+    //for(let j = 0; j < instruments.length; j++){
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+    //}
   }
   return array;
 }
